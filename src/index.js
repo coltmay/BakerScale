4ad97a2bc4b13abb4a2d6263b0d60a1ce18bb914
ColtMay <colten_mayberry@pm.me>
2021-04-23 14:40:13 -0500

    basic navbar, auth, and home page layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
+import { BakingApp } from './components/BakingApp';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <BakingApp />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
