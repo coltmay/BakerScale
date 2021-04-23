6fb03419cab6c69799f776d8b16842adaea239f5
ColtMay <colten_mayberry@pm.me>
2021-04-23 18:14:52 -0500

    wrote recipeList, using test render in recipeCard

---
+++ src/components/recipe/RecipeCard.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const EmployeeCard = ({ recipe }) => (
+        <h3>{recipe.name}</h3>
+)
--- src/components/recipe/RecipeList.js
+++ src/components/recipe/RecipeList.js
@@ -1,3 +1,31 @@
 import React, { useState, useEffect } from 'react'
+import { RecipeCard } from './RecipeCard'
 import { getAllRecipes } from '../../modules/RecipeManager'
 
+// A function to return recipes in a list format.
+export const RecipeList = () => {
+    // Declaring recipes as a state variable.
+    const [recipes, setRecipes] = useState([]);
+
+    // A function to get the recipes.
+    const getRecipes = () => {
+        return getAllRecipes()
+            .then(recipesFromAPI => {
+                setRecipes(recipesFromAPI);
+            });
+    };
+
+    // A useEffect invoke getRecipes
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
+    return (
+        <div>
+            {recipes.map(recipe => <RecipeCard
+                                    key={}
+                                    recipe={recipe}
+            />)}
+        </div>
+    )
+};
