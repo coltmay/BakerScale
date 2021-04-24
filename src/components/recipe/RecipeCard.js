4de7a45f8541ea99139aec21c816d0720720d5bb
ColtMay <colten_mayberry@pm.me>
2021-04-24 12:43:53 -0500

    added and colored icons

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React from "react";
+import editIcon from "../../images/editIcon.png";
+import deleteIcon from "../../images/deleteIcon.png";
 
 export const RecipeCard = ({ recipe }) => (
-        <h3>{recipe.title}</h3>
+        <section className="">
+                <h3>{recipe.title}</h3>
+                <p>{recipe.blurb}</p>
+                <img    className=""
+                        src={editIcon}
+                        onClick={console.log("edit")}></img>
+                <img    className=""
+                        src={deleteIcon}
+                        onClick={console.log("edit")}></img>
+        </section>
 )
