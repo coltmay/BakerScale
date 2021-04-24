ca38e332794c3ca199f7189cf015ee114afa60fd
ColtMay <colten_mayberry@pm.me>
2021-04-24 12:55:41 -0500

    edit icons link correctly

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import { getCurrentUser } from "../../components/helper/helperFunctions"
 import editIcon from "../../images/editIcon.png";
 import deleteIcon from "../../images/deleteIcon.png";
 
-export const RecipeCard = ({ recipe }) => (
-        <section className="">
-                <h3>{recipe.title}</h3>
-                <p>{recipe.blurb}</p>
-                <img    className=""
-                        src={editIcon}
-                        onClick={console.log("edit")}></img>
-                <img    className=""
-                        src={deleteIcon}
-                        onClick={console.log("edit")}></img>
-        </section>
-)
+export const RecipeCard = ({ recipe }) => {
+
+        return (
+                <section className="">
+                        <h3>{recipe.title}</h3>
+                        <p>{recipe.blurb}</p>
+                        <Link to={`/recipes/${recipe.id}/edit`}>
+                                <img className=""
+                                        src={editIcon}/>
+                        </Link>
+                        <img className=""
+                                src={deleteIcon}
+                                onClick={null} />
+                </section>
+        )
+}
