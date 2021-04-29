23fb9877095ce49b91cdaf6d2cc863477fd2c551
ColtMay <colten_mayberry@pm.me>
2021-04-29 15:19:51 -0500

    conversions happening from database pulls, but pulling ALL ingredients, fixing

--- src/components/recipe/RecipeDetailIngredientList.js
+++ src/components/recipe/RecipeDetailIngredientList.js
@@ -1,9 +1,39 @@
-export const RecipeIngredientsList = ({ ingredient }) => (
+export const RecipeIngredientsList = ({ ingredient }) => {
+    if (ingredient.measurement.id === 1) {
+        return <CupCard ingredient={ingredient}/>
+    } else if (ingredient.measurement.id === 2) {
+        return <TablespoonCard ingredient={ingredient}/>
+    } else if (ingredient.measurement.id === 3) {
+        return <TeaspoonCard ingredient={ingredient}/>
+    }
+}
+
+const CupCard = ({ ingredient }) => (
     <div className="detailIngredient">
         <p className="ingredientName">{ingredient.ingredient.name}</p>
         <div className="ingredientConvert">
             <p className="ingredientImperial">{ingredient.measurementAmount} cup(s)</p>
             <p className="ingredientGrams">{ingredient.measurementAmount * ingredient.ingredient.cupToGram} grams</p>
+        </div>
+    </div>
+)
+
+const TablespoonCard = ({ ingredient }) => (
+    <div className="detailIngredient">
+        <p className="ingredientName">{ingredient.ingredient.name}</p>
+        <div className="ingredientConvert">
+            <p className="ingredientImperial">{ingredient.measurementAmount} tablespoon(s)</p>
+            <p className="ingredientGrams">{ingredient.measurementAmount * ingredient.ingredient.tablespoonToGram} grams</p>
+        </div>
+    </div>
+)
+
+const TeaspoonCard = ({ ingredient }) => (
+    <div className="detailIngredient">
+        <p className="ingredientName">{ingredient.ingredient.name}</p>
+        <div className="ingredientConvert">
+            <p className="ingredientImperial">{ingredient.measurementAmount} teaspoon(s)</p>
+            <p className="ingredientGrams">{ingredient.measurementAmount * ingredient.ingredient.teaspoonToGram} grams</p>
         </div>
     </div>
 )
